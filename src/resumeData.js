ec4274aab0cb5afd6429360662d0f27684bde309
Brice Augustyn <baugustyn1000@gmail.com>
2018-09-20 18:12:35 -0700

    add space

--- src/resumeData.js
+++ src/resumeData.js
@@ -2,7 +2,7 @@ let resumeData = {
     // "imagebaseurl":"http://localhost:3000/",
     "imagebaseurl":"https://briceaugustyn.github.io/react-portfolio2/.",
     "name": "Brice",
-    "role": "Full Stack Web Developer",
+    "role": "Full Stack Web Developer ",
     "roleDescription": "creating awesome and effective visual application for companies of all sizes in California.",
     
       
