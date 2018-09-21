f1b4c9fe0a6a0b7e60034dc155891a0a46e74f25
Brice Augustyn <baugustyn1000@gmail.com>
2018-09-20 17:35:01 -0700

    first version

---
+++ src/resumeData.js
@@ -0,0 +1,11 @@
+let resumeData = {
+    // "imagebaseurl":"http://localhost:3000/",
+    "imagebaseurl":"https://briceaugustyn.github.io/react-portfolio2/.",
+    "name": "Briceeee",
+    "role": "Frontend Developer",
+    "roleDescription": "creating awesome and effective visual application for companies of all sizes in California.",
+    
+      
+          
+}
+  export default resumeData
