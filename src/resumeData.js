9cce855e2898c9aab7e15eeae6b885040401566d
Brice Augustyn <baugustyn1000@gmail.com>
2018-09-20 18:06:20 -0700

    added my name

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,8 +1,8 @@
 let resumeData = {
     // "imagebaseurl":"http://localhost:3000/",
     "imagebaseurl":"https://briceaugustyn.github.io/react-portfolio2/.",
-    "name": "Briceeee",
-    "role": "Frontend Developer",
+    "name": "Brice",
+    "role": "Full Stack Web Developer",
     "roleDescription": "creating awesome and effective visual application for companies of all sizes in California.",
     
       
