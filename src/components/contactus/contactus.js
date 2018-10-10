8cf00c26c9922501c5e032bd2a718c88aa99afcd
Brice Augustyn <baugustyn1000@gmail.com>
2018-10-09 23:44:00 -0700

    almost email

--- src/components/contactus/contactus.js
+++ src/components/contactus/contactus.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const encode = (data) => {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
 class ContactUs extends React.Component {
@@ -30,71 +30,81 @@ class ContactUs extends React.Component {
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
 
-    render() {
-      const { name, email, message } = this.state;
-      return (
-  
-        <section id="contact">
-          <div className="row section-head">
-            <div className="two columns header-col">
-              <h1><span>Get In Touch.</span></h1>
-            </div>
-            <div className="ten columns">
-              <p className="lead">LET'S GET IN TOUCH Tell me about yourself and what I can do for you.
-              </p>
-              <p className="lead">Feel free to contact me via email at: 
-              </p>
-              <p className="lead">
-              <a href="mailto:baugustyn1000@gmail.com?subject=Hello&body=We+want+to+hire+you">baugustyn1000@gmail.com</a>
-              </p>
-              <p className="lead">or drop a line in the form at the contact page
-              </p>
-               
-            </div>
+  render() {
+    const { name, email, message } = this.state;
+    return (
+
+      <section id="contact">
+        <div className="row section-head">
+          <div className="two columns header-col">
+            <h1><span>Get In Touch.</span></h1>
           </div>
-          <div className="row">
-            <div className="eight columns">
-              {/* form */}
-             
-             
-              <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-  {/* You still need to add the hidden input with the form name to your JSX form */}
-  <input type="hidden" name="form-name" value="contact" />
-
-</form>
-
-
-              <form onSubmit={this.handleSubmit}>
-              <p>
-                <label>
-                  Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-                </label>
+          <div className="ten columns">
+            <p className="lead">LET'S GET IN TOUCH Tell me about yourself and what I can do for you.
               </p>
-              <p>
-                <label>
-                  Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-                </label>
+            <p className="lead">Feel free to contact me via email at:
               </p>
-              <p>
-                <label>
+            <p className="lead">
+              <a href="mailto:baugustyn1000@gmail.com?subject=Hello&body=We+want+to+hire+you">baugustyn1000@gmail.com</a>
+            </p>
+            <p className="lead">or drop a line in the form at the contact page
+              </p>
+
+          </div>
+        </div>
+        <div className="row">
+          <div className="eight columns">
+            {/* form */}
+
+
+
+
+
+            {/* <label>
                   Message: <textarea name="message" value={message} onChange={this.handleChange} />
                 </label>
               </p>
               <p>
                 <button type="submit">Send</button>
               </p>
-            </form>
+            </form> */}
+
+            {/* <form action method="post" id="contactForm" name="contactForm" onSubmit={this.handleSubmit} >
+              <fieldset>
+
+                <div>
+                  <label htmlFor="contactName">Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Message: <textarea name="message" value={message} onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div>
+                  <button type="submit">Send</button>
+                </div>
+              </fieldset>
+            </form> */}
 
 
-              <form action method="post" id="contactForm" name="contactForm">
-                <fieldset>
+
+            <form action method="post" id="contactForm" name="contactForm" onSubmit={this.handleSubmit}>
+            <fieldset>
                   <div>
                     <label htmlFor="contactName">Name <span className="required">*</span></label>
-                    <input type="text" defaultValue size={35} id="contactName" name="contactName" />
+                    {/* <input type="text" defaultValue size={35} id="contactName" name="contactName" /> */}
+                    <input type="text" name="name" value={name} onChange={this.handleChange} />
                   </div>
                   <div>
                     <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                    <input type="text" defaultValue size={35} id="contactEmail" name="contactEmail" />
+                    {/* <input type="text" defaultValue size={35} id="contactEmail" name="contactEmail" /> */}
+                    <input type="email" name="email" value={email} onChange={this.handleChange} />
                   </div>
                   <div>
                     <label htmlFor="contactSubject">Subject</label>
@@ -110,48 +120,55 @@ class ContactUs extends React.Component {
                       <img alt src="images/loader.gif" />
                     </span>
                   </div>
+                  <div>
+                  <button type="submit">Send</button>
+                </div>
                 </fieldset>
-              </form> {/* Form End */}
-              {/* contact-warning */}
-              <div id="message-warning"> Error boy</div>
-              {/* contact-success */}
-              <div id="message-success">
-                <i className="fa fa-check" />Your message was sent, thank you!<br />
-              </div>
+                </form> 
+            {/* Form End */}
+
+
+
+            {/* contact-warning */}
+            <div id="message-warning"> Error boy</div>
+            {/* contact-success */}
+            <div id="message-success">
+              <i className="fa fa-check" />Your message was sent, thank you!<br />
             </div>
-            <aside className="four columns footer-widgets">
-              <div className="widget widget_contact">
-                <h4>Address and Phone</h4>
-                <p className="address">
-                  Brice Augustyn<br />
-                  606 Third Avenue <br />
-                  San Diego, CA 92101 US<br />
-                  <span>(707) 292 4556</span>
-                </p>
-              </div>
-              <div className="widget widget_tweets">
-                <h4 className="widget-title">Latest Tweets </h4>
-                <ul id="twitter">
-                  <li>
-                    <span>
-                      just building my new portfolio in reaasact fun #reactjs #hacking
-                      <a href="#"> http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b><a href="#">1 Days Ago</a></b>
-                  </li>
-                  <li>
-                    <span>
-                      I just went to this Python meet up and learn about data analysis so cool #python 
-                      <a href="#"> http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b><a href="#">3 Days Ago</a></b>
-                  </li>
-                </ul>
-              </div>
-            </aside>
           </div>
-        </section> 
-      );
-    }
-  };
-  export default ContactUs;
+          <aside className="four columns footer-widgets">
+            <div className="widget widget_contact">
+              <h4>Address and Phone</h4>
+              <p className="address">
+                Brice Augustyn<br />
+                606 Third Avenue <br />
+                San Diego, CA 92101 US<br />
+                <span>(707) 292 4556</span>
+              </p>
+            </div>
+            <div className="widget widget_tweets">
+              <h4 className="widget-title">Latest Tweets </h4>
+              <ul id="twitter">
+                <li>
+                  <span>
+                    just building my new portfolio in reaasact fun #reactjs #hacking
+                      <a href="#"> http://t.co/CGIrdxIlI3</a>
+                  </span>
+                  <b><a href="#">1 Days Ago</a></b>
+                </li>
+                <li>
+                  <span>
+                    I just went to this Python meet up and learn about data analysis so cool #python
+                      <a href="#"> http://t.co/CGIrdxIlI3</a>
+                  </span>
+                  <b><a href="#">3 Days Ago</a></b>
+                </li>
+              </ul>
+            </div>
+          </aside>
+        </div>
+      </section>
+    );
+  }
+};
+export default ContactUs;
