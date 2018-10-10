842dc8d0efe329d094fc3e5913e005dab32242c4
Brice Augustyn <baugustyn1000@gmail.com>
2018-10-09 21:42:15 -0700

    added email in react

--- src/components/contactus/contactus.js
+++ src/components/contactus/contactus.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
+
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
 class ContactUs extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+
     render() {
+      const { name, email, message } = this.state;
       return (
   
         <section id="contact">
@@ -20,14 +50,34 @@ class ContactUs extends React.Component {
               <p className="lead">or drop a line in the form at the contact page
               </p>
                
-              
-
-
             </div>
           </div>
           <div className="row">
             <div className="eight columns">
               {/* form */}
+
+              <form onSubmit={this.handleSubmit}>
+              <p>
+                <label>
+                  Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message: <textarea name="message" value={message} onChange={this.handleChange} />
+                </label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
+
+
               <form action method="post" id="contactForm" name="contactForm">
                 <fieldset>
                   <div>
