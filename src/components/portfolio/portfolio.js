8c840c01d7ac322c1203fe693ce07cb395139798
Brice Augustyn <baugustyn1000@gmail.com>
2018-11-23 17:08:14 -0800

    missing SD Discount window

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -233,66 +233,7 @@ class Portfolio extends React.Component {
             <a className="popup-modal-dismiss">Close</a>
           </div>
         </div>{/* modal-03 End */}
-        <div id="modal-04" className="popup-modal mfp-hide">
-          <img className="scale-with-grid" src="images/portfolio/modals/m-intothelight.jpg" alt />
-          <div className="description-box">
-            <h4>Into the Light</h4>
-            <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-            <span className="categories"><i className="fa fa-tag" />Photography</span>
-          </div>
-          <div className="link-box">
-            <a href="http://www.behance.net">Details</a>
-            <a className="popup-modal-dismiss">Close</a>
-          </div>
-        </div>{/* modal-04 End */}
-        <div id="modal-05" className="popup-modal mfp-hide">
-          <img className="scale-with-grid" src="images/portfolio/modals/m-farmerboy.jpg" alt />
-          <div className="description-box">
-            <h4>Farmer Boy</h4>
-            <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-            <span className="categories"><i className="fa fa-tag" />Branding, Webdesign</span>
-          </div>
-          <div className="link-box">
-            <a href="http://www.behance.net">Details</a>
-            <a className="popup-modal-dismiss">Close</a>
-          </div>
-        </div>{/* modal-05 End */}
-        <div id="modal-06" className="popup-modal mfp-hide">
-          <img className="scale-with-grid" src="images/portfolio/modals/m-girl.jpg" alt />
-          <div className="description-box">
-            <h4>Girl</h4>
-            <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-            <span className="categories"><i className="fa fa-tag" />Photography</span>
-          </div>
-          <div className="link-box">
-            <a href="http://www.behance.net">Details</a>
-            <a className="popup-modal-dismiss">Close</a>
-          </div>
-        </div>{/* modal-06 End */}
-        <div id="modal-07" className="popup-modal mfp-hide">
-          <img className="scale-with-grid" src="images/portfolio/modals/m-origami.jpg" alt />
-          <div className="description-box">
-            <h4>Origami</h4>
-            <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-            <span className="categories"><i className="fa fa-tag" />Branding, Illustration</span>
-          </div>
-          <div className="link-box">
-            <a href="http://www.augustynbrice.com/test/">Details</a>
-            <a className="popup-modal-dismiss">Close</a>
-          </div>
-        </div>{/* modal-07 End */}
-        <div id="modal-08" className="popup-modal mfp-hide">
-          <img className="scale-with-grid" src="images/portfolio/modals/m-retrocam.jpg" alt />
-          <div className="description-box">
-            <h4>Retrocam</h4>
-            <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-            <span className="categories"><i className="fa fa-tag" />Webdesign, Photography</span>
-          </div>
-          <div className="link-box">
-            <a href="http://www.augustynbrice.com/test/">Details</a>
-            <a className="popup-modal-dismiss">Close</a>
-          </div>
-        </div>{/* modal-01 End */}
+       
           </div> {/* row End */ }
         </section > 
       );
